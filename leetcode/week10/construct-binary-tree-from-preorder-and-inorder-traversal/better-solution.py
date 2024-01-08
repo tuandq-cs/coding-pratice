c84a63499af7a7859db96d014a10e1b91446ad82
tuandq-cs <tuandq.cs@gmail.com>
2024-01-09 06:40:03 +0700

    week10

--- leetcode/week10/construct-binary-tree-from-preorder-and-inorder-traversal/better-solution.py
+++ leetcode/week10/construct-binary-tree-from-preorder-and-inorder-traversal/better-solution.py
@@ -11,14 +11,17 @@ class TreeNode:
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         m = {}
-        for i, num in inorder:
+        for i, num in enumerate(inorder):
             m[num] = i
 
         def helper(preId: int, inStart: int, inEnd: int):
             # Condition here
+            if not (0 <= preId < len(preorder)) or not (0 <= inStart <= inEnd < len(inorder)):
+                return None
             rootId = m[preorder[preId]]
             numLeftEle = rootId - inStart
             root = TreeNode(preorder[preId])
             root.left = helper(preId+1, inStart, rootId-1)
-            root.right = helper(preId+numLeftEle, rootId+1, inEnd)
-            
+            root.right = helper(preId+1+numLeftEle, rootId+1, inEnd)
+            return root
+        return helper(0, 0, len(inorder) - 1)
