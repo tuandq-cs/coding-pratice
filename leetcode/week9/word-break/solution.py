daa1f3360af714e6f3b63080cf1c14ddfe9b3dbd
tuandq-cs <tuandq.cs@gmail.com>
2024-01-15 21:04:50 +0700

    word break

---
+++ leetcode/week9/word-break/solution.py
@@ -0,0 +1,40 @@
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        m = {}
+        for _, word in enumerate(wordDict):
+            m[word] = True
+
+        def solve(s: str):
+            if m.get(s) is not None:
+                return True
+            for i in range(len(s)-1):
+                if m.get(s[:i+1]) is not None and solve(s[i+1:]):
+                    return True
+            return False
+        # Time: O(n^2 + m)
+        # Space: O(m)
+        # n is length of s
+        # m is length of wordDict
+        return solve(s)
+s = "leetcode"
+wordDict = ["leet","code"]
+out = Solution().wordBreak(s, wordDict)
+print(out)
+
+s = "applepenapple"
+wordDict = ["apple","pen"]
+out = Solution().wordBreak(s, wordDict)
+print(out)
+
+s = "catsandog"
+wordDict = ["cats","dog","sand","and","cat"]
+out = Solution().wordBreak(s, wordDict)
+print(out)
+
+s = "c"
+wordDict = ["cats","dog","sand","and","cat"]
+out = Solution().wordBreak(s, wordDict)
+print(out)
