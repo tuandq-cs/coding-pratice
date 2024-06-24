1d9c2b8f7c3ec685ceebbdf3f46495cc8f5e3d3b
Tuan Dong Quoc <tuandq.cs@gmail.com>
2024-06-24 21:16:49 +0700

    minimum-number-of-k-consecutive-bit-flips

---
+++ leetcode/minimum-number-of-k-consecutive-bit-flips/optimal-solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    # https://leetcode.com/problems/minimum-number-of-k-consecutive-bit-flips/solutions/238609/java-c-python-one-pass-and-o-1-space/comments/236804
+    def minKBitFlips(self, nums: List[int], k: int) -> int:
+        # [0, 1, 0, 0, 1, 1, 1, 0, 1, 0], k = 4
+        n = len(nums)
+        numFlipsWin = 0 # total flip times at current fixed k-length window
+        ans = 0
+        for i in range(n):
+            if i >= k:
+                if nums[i-k] == 2: # remove the i-k from window
+                    numFlipsWin -= 1
+
+            if numFlipsWin % 2 == nums[i]: # need to flip at current i
+                # if we still flip at i and i + k > n that mean slip useless
+                if i + k > n:
+                    return -1
+                numFlipsWin += 1
+                ans += 1
+                nums[i] = 2
+        return ans
+                
